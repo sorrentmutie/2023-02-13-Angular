44be2950b4aa209fcee1fb5a87cd103f3663684f
salvatore <salvatore.sorrentino@live.com>
2023-02-14 13:33:16 +0100

    observables

--- src/app/shared/footer/footer.component.ts
+++ src/app/shared/footer/footer.component.ts
@@ -1,5 +1,7 @@
-import { Component, Input, OnChanges, SimpleChanges } from '@angular/core';
+import { Component, Input, OnChanges, OnDestroy, SimpleChanges } from '@angular/core';
+import { filter, map, of, Subscription } from 'rxjs';
 import { ProductsService } from 'src/app/products/services/products.service';
+import { Order } from '../order';
 
 @Component({
   selector: 'app-footer',
@@ -7,16 +9,34 @@ import { ProductsService } from 'src/app/products/services/products.service';
   styleUrls: ['./footer.component.css'],
   providers: [ ProductsService]
 })
-export class FooterComponent implements OnChanges {
+export class FooterComponent implements OnChanges,
+                                        OnDestroy {
   @Input() text:string = ""; 
+  private subscription: Subscription | null = null;
 
   constructor(public service: ProductsService) {}
+  ngOnDestroy(): void {
+    this.subscription?.unsubscribe();
+  }
 
   ngOnChanges(changes: SimpleChanges): void {
     console.log(changes);
   }
 
-
-
-
+  doSomething() {
+    const myObservable = of(1,2,3,4,5,6,7,8,9,10);
+    const mySubscriber = {
+      next: (x: any) => console.log(x),
+      error: (error: Error) => console.log(error),
+      complete: () => console.log('Trasmissione terminata')
+    };
+    if(!this.subscription) {
+      this.subscription = myObservable
+      .pipe(
+        filter( x => x % 2 ===0),
+        map(x => x * 500  ),
+        map(x => new Order(x)  ))
+      .subscribe(mySubscriber);
+    }
+  }
 }
---
+++ src/app/shared/order.ts
@@ -0,0 +1,3 @@
+export class Order {
+    constructor(public id: number) {    }
+}
